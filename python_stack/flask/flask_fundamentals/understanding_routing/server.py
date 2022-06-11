589ffcf24b2add829a5ddcb5381f9610e63d5a86
chaosxd13 <albaraa.o.badawi@gmail.com>
2022-06-11 19:07:49 +0300

    update

--- python_stack/flask/flask_fundamentals/understanding_routing/server.py
+++ python_stack/flask/flask_fundamentals/understanding_routing/server.py
@@ -13,9 +13,12 @@ def dojo():
 def say(text):
     return f"<h1>Hi {text}!</h1>"
 
-@app.route('/repeat/<num>/<text>')
+@app.route('/repeat/<int:num>/<text>')
 def repeat(num, text):
     return f"<h2>{text}</h2>"*int(num)
 
+@app.errorhandler(404) 
+def invalid_route(e):
+    return "<h1>Sorry! No response. Try again.</h1>"
 if __name__=="__main__":
     app.run(debug=True)
