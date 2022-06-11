1580f23735e02b59ce8ccd64fd0e170d49153cd9
chaosxd13 <albaraa.o.badawi@gmail.com>
2022-06-12 02:13:22 +0300

    update

--- python_stack/flask/flask_fundamentals/checkerboard/server.py
+++ python_stack/flask/flask_fundamentals/checkerboard/server.py
@@ -4,15 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def size8by8():
-    return render_template("index.html", x=8, y=8)
+    return render_template("index.html", x=8, y=8, color1="red", color2="black")
 
 @app.route('/<int:x>')
 def size8byy(x):
-    return render_template("index.html", x=x, y=8)
+    return render_template("index.html", x=x, y=8, color1="red", color2="black")
 
 @app.route('/<int:x>/<int:y>')
 def sizexbyy(x, y):
-    return render_template("index.html", x=x, y=y)
+    return render_template("index.html", x=x, y=y, color1="red", color2="black")
+
+@app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
+def sizexbyy_color(x, y, color1, color2):
+    return render_template("index.html", x=x, y=y, color1=color1, color2=color2)
 
 if __name__=="__main__":
     app.run(debug=True)
